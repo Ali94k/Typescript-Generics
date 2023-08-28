02bf89a3947b72e5e2e760ee3d21f48c78f32c03
Ali <alikerimli94@gmail.com>
2023-08-28 14:57:26 +0200

    Constraints

--- src/app.ts
+++ src/app.ts
@@ -17,4 +17,11 @@ const mergedObj2 = merge2({name: 'Max'}, {age: 30})
 
 mergedObj2.age // No Error: Property 'age' does not exist on type 'object'
 
+// Constraints
 
+function merge3<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergedObj3 = merge2({name: 'Max', hobbies: ['Sports']}, 30) //  fails silently
+const mergedObj4 = merge3({name: 'Max', hobbies: ['Sports']}, 30) //  fails with error
