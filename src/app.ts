a2bc4d875a8cf02e2a9055005f7c80a2b124846e
Ali <alikerimli94@gmail.com>
2023-08-28 15:39:53 +0200

    Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -54,3 +54,37 @@ function extractAndConvert<T extends object, U extends keyof T>(
 ) {
   return 'Value: ' + obj[key]
 }
+
+
+// Generic Classes
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1) // -1
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+
+textStorage.removeItem('Max')
+
+const numberAndTextStorage = new DataStorage<number | string>()
+
+numberAndTextStorage.addItem('Max')
+numberAndTextStorage.addItem(1)
