80129555381f65e790aafe3a747d293d05598a4f
Ali <alikerimli94@gmail.com>
2023-08-28 15:45:50 +0200

    Generic Utility Types

--- src/app.ts
+++ src/app.ts
@@ -88,3 +88,34 @@ const numberAndTextStorage = new DataStorage<number | string>()
 
 numberAndTextStorage.addItem('Max')
 numberAndTextStorage.addItem(1)
+
+
+// Generic Utility Types
+
+// Partial
+
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  // Partial
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+  return courseGoal as CourseGoal
+}
+
+// Readonly
+
+const names: Readonly<string[]> = ['Max', 'Anna']
+// names.push('Manu') // Error: Property 'push' does not exist on type 'readonly string[]'
+
+
